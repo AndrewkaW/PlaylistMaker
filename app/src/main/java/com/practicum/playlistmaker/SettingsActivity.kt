0a41734b3ec503bdcb25da07037d35f2cef0222b
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-02-13 15:32:22 +0300

    Исприавил мелкие недочеты. в SettingsActivity перешел на ToolBar

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -3,13 +3,14 @@ package com.practicum.playlistmaker
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-
-        findViewById<ImageView>(R.id.button_back_to_main).setOnClickListener {
+        
+        findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_id).setOnClickListener {
             finish()
         }
     }
