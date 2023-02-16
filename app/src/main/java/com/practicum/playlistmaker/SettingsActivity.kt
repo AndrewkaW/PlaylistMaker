7e025b27d799ba0a2726d5a19a481af1e7cd0fd3
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-02-16 16:17:00 +0300

    Добавил Темную тему и стили

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,14 +2,12 @@ package com.practicum.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        
+
         findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar_id).setOnClickListener {
             finish()
         }
