dd942c600a0a18cde4ce635ac125679ae6fe0724
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-03-12 11:23:16 +0300

    TEST COMMIT

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -62,7 +62,7 @@ class SearchActivity : AppCompatActivity() {
             Track("Sweet Child O'Mine","Guns N' Roses","5:03",
                 "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg")
         )
-
+//TEST COMMIT
 
         inputEditText = findViewById(R.id.inputEditText)
         inputEditText.addTextChangedListener(searchTextWatcher)
