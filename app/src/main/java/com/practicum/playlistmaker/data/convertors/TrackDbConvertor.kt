70307b9ce96255d2891f5e9bb7f7294003f048be
Andrey_W <Uzzbagoyzza@yandex.ru>
2023-12-20 17:09:04 +0300

    Удалил коменты и пробелы

--- app/src/main/java/com/practicum/playlistmaker/data/convertors/TrackDbConvertor.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/convertors/TrackDbConvertor.kt
@@ -37,5 +37,4 @@ class TrackDbConvertor {
             track.isFavorite,
         )
     }
-
 }
--- app/src/main/java/com/practicum/playlistmaker/data/search/network/model/TrackDto.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/search/network/model/TrackDto.kt
@@ -1,15 +1,15 @@
 package com.practicum.playlistmaker.data.search.network.model
 
 data class TrackDto(
-    val trackId: Int, // уникальный идентификатор трека
-    val trackName: String, // Название композиции
-    val artistName: String?, // Имя исполнителя
-    val trackTimeMillis: Int?, // Продолжительность трека
+    val trackId: Int,
+    val trackName: String,
+    val artistName: String?,
+    val trackTimeMillis: Int?,
     val artworkUrl100: String?,
-    val artworkUrl60: String?, // Ссылка на изображение обложки
-    val collectionName: String?, // Название альбома
-    val releaseDate: String?, // Год релиза трека
-    val primaryGenreName: String?, // Жанр трека
-    val country: String?, // Страна исполнителя
-    val previewUrl: String?, // Ссылка на отрывок
+    val artworkUrl60: String?,
+    val collectionName: String?,
+    val releaseDate: String?,
+    val primaryGenreName: String?,
+    val country: String?,
+    val previewUrl: String?,
 )
